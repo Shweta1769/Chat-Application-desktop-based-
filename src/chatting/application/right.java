1df26c26e4442738dc574c524ed821b30f13c04e
Shweta Kapat <133523915+Shweta1769@users.noreply.github.com>
2024-05-16 10:41:49 +0530

    Add files via upload

---
+++ src/chatting/application/right.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chatting.application;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Shweta
+ */
+class right {
+
+    static void add(JPanel p2, String LINE_END) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
